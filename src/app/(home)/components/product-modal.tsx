b900150f8a2aa4dc5060e5c2f4bd40eb975ec07d
zahid-soft <zahidsarang95@gmail.com>
2024-06-16 22:47:16 +0530

    hide toppings for other categories

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -113,9 +113,13 @@ const ProductModal = ({ product }: { product: Product }) => {
 								);
 							})}
 
-							<Suspense fallback={"Toppings Loading...."}>
-								<ToppingList selectedToppings={selectedToppings} handleCheckBoxCheck={handleCheckBoxCheck} />
-							</Suspense>
+							{/* todo: make this condition dynamic */}
+
+							{product.category.name === "pizza" && (
+								<Suspense fallback={"Toppings Loading...."}>
+									<ToppingList selectedToppings={selectedToppings} handleCheckBoxCheck={handleCheckBoxCheck} />
+								</Suspense>
+							)}
 
 							<div className="flex items-center justify-between mt-12">
 								<span className="font-bold">₹{totalPrice}</span>
