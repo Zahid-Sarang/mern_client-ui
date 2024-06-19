a96c3e7798fa0ce0965ab4f4168bb0914efecb91
zahid-soft <zahidsarang95@gmail.com>
2024-06-19 12:25:15 +0530

    chnage cart item qty

--- src/lib/store/features/cart/cartSlice.ts
+++ src/lib/store/features/cart/cartSlice.ts
@@ -13,7 +13,6 @@ export interface CartItem extends Pick<Product, "_id" | "name" | "image" | "pric
 	qty: number;
 	hash?: string;
 }
-
 export interface CartState {
 	cartItems: CartItem[];
 }
@@ -34,6 +33,7 @@ export const cartSlice = createSlice({
 				// product: action.payload.product,
 				// chosenConfiguration: action.payload.chosenConfiguration,
 			};
+
 			window.localStorage.setItem("cartItems", JSON.stringify([...state.cartItems, newItem]));
 			return {
 				cartItems: [...state.cartItems, newItem],
@@ -42,8 +42,17 @@ export const cartSlice = createSlice({
 		setInitialCartItems: (state, action: PayloadAction<CartItem[]>) => {
 			state.cartItems.push(...action.payload);
 		},
+		changeQty: (state, action: PayloadAction<{ hash: string; qty: number }>) => {
+			const index = state.cartItems.findIndex((item) => item.hash === action.payload.hash);
+
+			state.cartItems[index].qty = Math.max(1, state.cartItems[index].qty + action.payload.qty);
+
+			window.localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+		},
 	},
 });
 
-export const { addToCart, setInitialCartItems } = cartSlice.actions;
+// Action creators are generated for each case reducer function
+export const { addToCart, setInitialCartItems, changeQty } = cartSlice.actions;
+
 export default cartSlice.reducer;
