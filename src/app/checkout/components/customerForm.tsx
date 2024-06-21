ac5255d9d7ae8f1e45250b23999070b8c93f06f8
zahid-soft <zahidsarang95@gmail.com>
2024-06-21 16:42:29 +0530

    render customer addresses

--- src/app/checkout/components/customerForm.tsx
+++ src/app/checkout/components/customerForm.tsx
@@ -16,12 +16,13 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 import { getCustomer } from "@/lib/http/api";
+import { Customer } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 import { Coins, CreditCard, Plus } from "lucide-react";
 import React from "react";
 
 const CustomerForm = () => {
-	const { data: customer, isLoading } = useQuery({
+	const { data: customer, isLoading } = useQuery<Customer>({
 		queryKey: ["customer"],
 		queryFn: async () => {
 			return await getCustomer().then((res) => res.data);
@@ -45,15 +46,15 @@ const CustomerForm = () => {
 					<div className="grid gap-6">
 						<div className="grid gap-3">
 							<Label htmlFor="fname">First Name</Label>
-							<Input id="fname" type="text" className="w-full" defaultValue={customer.firstName} disabled />
+							<Input id="fname" type="text" className="w-full" defaultValue={customer?.firstName} disabled />
 						</div>
 						<div className="grid gap-3">
 							<Label htmlFor="lname">Last Name</Label>
-							<Input id="lname" type="text" className="w-full" defaultValue={customer.lastName} disabled />
+							<Input id="lname" type="text" className="w-full" defaultValue={customer?.lastName} disabled />
 						</div>
 						<div className="grid gap-3">
 							<Label htmlFor="email">Email</Label>
-							<Input id="email" type="text" className="w-full" defaultValue={customer.email} disabled />
+							<Input id="email" type="text" className="w-full" defaultValue={customer?.email} disabled />
 						</div>
 						<div className="grid gap-3">
 							<div>
@@ -84,22 +85,27 @@ const CustomerForm = () => {
 									</Dialog>
 								</div>
 								<RadioGroup defaultValue="option-one" className="grid grid-cols-2 gap-6 mt-2">
-									<Card className="p-6">
-										<div className="flex items-center space-x-2">
-											<RadioGroupItem value="option-one" id="option-one" />
-											<Label htmlFor="option-one" className="leading-normal">
-												123, ABC Street, Malad West, Mumbai, Maharashtra, India 400064
-											</Label>
-										</div>
-									</Card>
-									<Card className="p-6">
+									{customer?.addresses.map((address) => {
+										return (
+											<Card className="p-6" key={address.text}>
+												<div className="flex items-center space-x-2">
+													<RadioGroupItem value="option-one" id="option-one" />
+													<Label htmlFor="option-one" className="leading-normal">
+														{address.text}
+													</Label>
+												</div>
+											</Card>
+										);
+									})}
+
+									{/* <Card className="p-6">
 										<div className="flex items-center space-x-2">
 											<RadioGroupItem value="option-two" id="option-two" />
 											<Label htmlFor="option-two" className="leading-normal">
 												Flat No. 501, Sunshine Apartments, Andheri East, Mumbai, Maharashtra, India 400069
 											</Label>
 										</div>
-									</Card>
+									</Card> */}
 								</RadioGroup>
 							</div>
 						</div>
