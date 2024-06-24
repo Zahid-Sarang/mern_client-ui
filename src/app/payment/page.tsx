d93bfbff5b64a24cb84d85fb5ca30d1de0ca8d6b
zahid-soft <zahidsarang95@gmail.com>
2024-06-24 16:37:41 +0530

    add restaurant id to url

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -6,7 +6,7 @@ import { ArrowLeft, CheckCircle2, CircleX, LayoutDashboard, Store } from "lucide
 import Link from "next/link";
 import React from "react";
 
-const Payment = ({ searchParams }: { searchParams: { success: string; orderId: string } }) => {
+const Payment = ({ searchParams }: { searchParams: { success: string; orderId: string; restaurantId: string } }) => {
 	const isOrderSuccess = searchParams.success === "true";
 
 	return (
@@ -59,16 +59,17 @@ const Payment = ({ searchParams }: { searchParams: { success: string; orderId: s
 
 			{isOrderSuccess ? (
 				<Button asChild className="mt-6">
-					{/* todo: get tenantId from redirect url add it on backend */}
-					<Link href={`/order-status/${searchParams.orderId}`} className="flex items-center gap-2">
+					<Link
+						href={`/order-status/${searchParams.orderId}?restaurantId=${searchParams.restaurantId}`}
+						className="flex items-center gap-2"
+					>
 						<ArrowLeft size={20} className="text-white" />
 						<span>Check order status</span>
 					</Link>
 				</Button>
 			) : (
 				<Button asChild className="mt-6">
-					{/* todo: get tenantId from redirect url add it on backend */}
-					<Link href="/checkout" className="flex items-center gap-2">
+					<Link href={`/checkout?restaurantId=${searchParams.restaurantId}`} className="flex items-center gap-2">
 						<ArrowLeft size={20} className="text-white" />
 						<span>Go to checkout</span>
 					</Link>
